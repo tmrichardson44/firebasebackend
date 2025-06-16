4efc6189886add8fa643d0ec9bdb254a97424206
Tom Richardson <tmrichardson44@gmail.com>
2025-06-16 12:03:05 -0400

    Add firebase Cloud Functions folder

---
+++ functions/index.js
@@ -0,0 +1,30 @@
+/**
+ * Import function triggers from their respective submodules:
+ *
+ * const {onCall} = require("firebase-functions/v2/https");
+ * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
+ *
+ * See a full list of supported triggers at https://firebase.google.com/docs/functions
+ */
+
+const {onRequest} = require("firebase-functions/v2/https");
+const logger = require("firebase-functions/logger");
+
+const functions = require('firebase-functions');
+const admin = require('firebase-admin'); // If you're using admin SDK, otherwise remove
+admin.initializeApp(); // Initialize admin SDK if needed
+
+// EXPORT your function
+exports.myFirstFunction = functions.https.onRequest((request, response) => {
+  functions.logger.info("Hello from my first Cloud Function!", {structuredData: true});
+  response.send("Hello from Firebase Cloud Functions!");
+});
+
+// Create and deploy your first functions
+// https://firebase.google.com/docs/functions/get-started
+
+// exports.helloWorld = onRequest((request, response) => {
+//   logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
+
